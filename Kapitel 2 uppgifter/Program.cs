0114b72bc337ad2d92e300b26b2d381d1444f84f
E-Svensson <111729734+CarlHellborgsson@users.noreply.github.com>
2022-09-09 10:33:15 +0200

    Add project files.

---
+++ Kapitel 2 uppgifter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Kapitel_2_uppgifter
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Vilken är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine(bok);
+        }
+    }
+}
